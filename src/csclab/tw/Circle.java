41387d21354bab8de9020fac118ed7fff43cabff
tinghan33704 <tinghan33704@gmail.com>
2014-12-02 17:14:26 +0800

    Finish Circle.java

--- src/csclab/tw/Circle.java
+++ src/csclab/tw/Circle.java
@@ -1,17 +1,16 @@
 package csclab.tw;
 
 public class Circle extends Shape {
-	
+
 	private double radius;
-	
+
 	public Circle(double radius){
 		this.radius = radius;
 	}
-	
+
 	@Override
 	public double getArea() {
-		// TODO Auto-generated method stub
-		return 0;
+		return radius*radius*3.14;
 	}
 
 }
